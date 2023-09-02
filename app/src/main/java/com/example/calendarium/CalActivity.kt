edb97e9a8a3d2b86a58c9a948d9b384e98d68af8
onehalfgemini <140209294+onehalfgemini@users.noreply.github.com>
2023-09-02 15:32:43 +0200

    Zmiana działania przycisku wyloguj

--- app/src/main/java/com/example/calendarium/CalActivity.kt
+++ app/src/main/java/com/example/calendarium/CalActivity.kt
@@ -7,12 +7,14 @@ import com.example.calendarium.databinding.ActivityCalBinding
 import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.CalendarView.OnDateChangeListener
+import com.google.firebase.auth.FirebaseAuth
 
 class CalActivity : AppCompatActivity() {
 
     private lateinit var calendarView: CalendarView //*
     private lateinit var dateTV: TextView
     private lateinit var binding: ActivityCalBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +26,16 @@ class CalActivity : AppCompatActivity() {
         calendarView = findViewById(R.id.calendarView)
 
         binding.btnLogout.setOnClickListener{
-            val logoutIntent = Intent(this,LogInActivity::class.java)
+            FirebaseAuth.getInstance().signOut()
+
+            val logoutIntent = Intent(this, LogInActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(logoutIntent)
+            finish()
         }
+
+
+
         calendarView.setOnDateChangeListener(
             OnDateChangeListener { view, year, month, dayOfMonth ->
                 // In this Listener we are getting values
